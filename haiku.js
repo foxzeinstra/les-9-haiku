a689dcd97f5f7ca6d95b4005fcc6a936aca2e23f
fox zeinstra <fox.zeinstra@gmail.com>
2023-10-09 12:02:52 +0200

    .

---
+++ haiku.js
@@ -0,0 +1,26 @@
+
+
+//plak hier de haiku functions onder elkaar
+function HaikuX(){
+    console.log("haiku by:","fox");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","maaike");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+function HaikuX(){
+    console.log("haiku by:","ole");
+    console.log("REGEL1");
+    console.log("REGEL2");
+    console.log("REGEL3");
+}
+
+//laat de code hieronder met rust
+Haiku1()
+Haiku2()
+Haiku3()
